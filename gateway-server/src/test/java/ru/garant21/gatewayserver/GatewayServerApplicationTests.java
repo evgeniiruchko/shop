c38dbc163693e41e0b1199a8747e8ea28b739334
EvgeniiRuchko <gk1987@mail.ru>
2021-08-25 20:59:02 +0300

    gateway и изменения в структуре

---
+++ gateway-server/src/test/java/ru/garant21/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.garant21.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- ms-orders/src/main/java/ru/garant21/orders/services/OrderClient.java
+++ ms-orders/src/main/java/ru/garant21/orders/services/OrderClient.java
@@ -2,7 +2,7 @@ package ru.garant21.orders.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.garant21.products.dtos.ProductDto;
 
 import java.util.List;
@@ -10,5 +10,5 @@ import java.util.List;
 @FeignClient("ms-products")
 public interface OrderClient {
     @GetMapping("/get-from-orders")
-    public List<ProductDto> getProductsByListId(@RequestBody List<Long> listId);
+    List<ProductDto> getProductsByListId(@RequestParam List<Long> listId);
 }
